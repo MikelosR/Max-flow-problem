c11a7c5236ac0830d66ef271bbe2a104f25e0575
MikelosR <miiikelossspap@outlook.com.gr>
2025-02-17 01:12:28 +0200

    third commit

--- main_max_flow_problem.cpp
+++ main_max_flow_problem.cpp
@@ -14,17 +14,15 @@ int main() {
     //Variables to hold the three numbers
     int a, b;
     unsigned long c; 
-    int num_of_idragogeia = 500, solines = 1000;
+    int num_of_idragogeia = 500, agogoi = 1000;
     
     vector<tuple<int, int, int>> all_edges_vector;
     string line;
     unsigned long upper_bound = 1000000000;
     while (getline(file, line)) { // Read each line from the file
-        //cout<<line<<endl;
         istringstream iss(line); // Create a string stream from the line
         if (iss>>a>>b>>c) { // Extract the three numbers from the line
             all_edges_vector.emplace_back(a, b, c); // Store the numbers in a vector as a tuple
-            //cout<<"Read: "<<a<<" "<<b<<" "<<c<<endl;
             //Restriction a,b,c
             if(a < 1 || a > num_of_idragogeia) cout<<"wrong a index!"<<endl;
             if(b < 1 || b > num_of_idragogeia) cout<<"wrong b index!"<<endl;
@@ -34,11 +32,10 @@ int main() {
         else{
             iss.clear(); //Clear the stream's fail state
             iss.str(line); //Reset the stream with the current line
-            if (iss >> num_of_idragogeia >> solines) {
-                //cout << "Read: " << num_of_idragogeia << " " << solines << endl;
-                //Restriction num_of_idragogeia, solines
+            if (iss >> num_of_idragogeia >> agogoi) {
+                //Restriction num_of_idragogeia, agogoi
                 if(num_of_idragogeia < 1 || num_of_idragogeia > 500) cout<<"polla idragogeia!"<<endl;
-                if(solines < 1 || solines > 1000) cout<<"polloi solines!"<<endl;
+                if(agogoi < 1 || agogoi > 1000) cout<<"polloi agogoi!"<<endl;
             } 
             else {
                 cerr << "Error: Invalid line format." << endl;
@@ -46,7 +43,7 @@ int main() {
         }
     }
 
-    file.close(); // Close the file
+    file.close(); //Close the file
 
     vector<ydragogeio> graph(num_of_idragogeia+1);
     agogos first_index;
@@ -58,7 +55,7 @@ int main() {
     agogos temp_edge;
     for (const auto& tpl : all_edges_vector) {
         int a, b, c;
-        tie(a, b, c) = tpl; // Extract values from the tuple
+        tie(a, b, c) = tpl; //Extract values from the tuple
         temp_edge.set_capacity(c);
         temp_edge.set_flow(0);
         temp_edge.set_from_idragogeio(a);
@@ -66,14 +63,8 @@ int main() {
         graph[a].add_edge(temp_edge);
     }
 
-    //vector<ydragogeio> residualGraph = create_residual_graph(graph);
-    /*for (int i =  0; i <= num_of_idragogeia; i++){
-        cout<<"Idragogeio noumero: "<<residualGraph[i].get_name()<<endl;
-        residualGraph[i].print_edges();
-    }*/
     int max_flow = ford_fulkerson(graph, 1, graph.size()-1);
-    cout << "Maximum Flow: " << max_flow << endl;
-
+    cout<<"Maximum Flow: "<<max_flow<<endl;
 	return 0;
 }
 
